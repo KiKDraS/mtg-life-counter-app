cd774f14ced902c349bc01c6b44489b27bbab755
Anahí Foresi <anahiforesi@gmail.com>
2026-07-21 15:57:07 -0300

    Fix clan symbol

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import ManaSelector from "@/components/ui/icons/ManaSelector";
 import GuildSelector from "@/components/ui/icons/GuildSelector";
 import ClanSelector from "@/components/ui/icons/ClanSelector";
 import ShardSelector from "@/components/ui/icons/ShardSelector";
+import CounterSelector from "@/components/ui/icons/CounterSelector";
 
 export const metadata: Metadata = {
   title: "MTG Life Counter",
@@ -23,7 +24,7 @@ export default function Home() {
         />
       </div>
       <div className="flex gap-1">
-        <ClanSelector clan="abzan" />
+        <CounterSelector counter="energy" />
       </div>
       <div className="flex-1">
         <PlayerZone playerNumber={2} color="r" initialLife={40} />
