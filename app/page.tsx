7a928b85c6f04677427c4eef8e43c94304513c4f
Anahí Foresi <anahiforesi@gmail.com>
2026-07-21 16:00:10 -0300

    Fix energy symbol

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,5 @@
 import type { Metadata } from "next";
 import { PlayerZone } from "@/components/game/player-zone";
-import ManaSelector from "@/components/ui/icons/ManaSelector";
-import GuildSelector from "@/components/ui/icons/GuildSelector";
-import ClanSelector from "@/components/ui/icons/ClanSelector";
-import ShardSelector from "@/components/ui/icons/ShardSelector";
-import CounterSelector from "@/components/ui/icons/CounterSelector";
 
 export const metadata: Metadata = {
   title: "MTG Life Counter",
@@ -22,9 +17,6 @@ export default function Home() {
           rotation={180}
           initialLife={40}
         />
-      </div>
-      <div className="flex gap-1">
-        <CounterSelector counter="energy" />
       </div>
       <div className="flex-1">
         <PlayerZone playerNumber={2} color="r" initialLife={40} />
